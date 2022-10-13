c2e95bf8d5e985f508f8d7c43893b1cb63bce1dc
MikeDark <mkfist2911@gmail.com>
2022-10-13 21:24:53 +0300

    новые данные

--- Пример2/Program.cs
+++ Пример2/Program.cs
@@ -1,5 +1,7 @@
 ﻿Console.Write("Введите ваше имя");
 string username = Console.ReadLine();
+Console.Write("Введите ваш Возраст");
+string userage = Console.ReadLine();
 Console.Write("Привет,");
 Console.Write(username);
-
+Console.Write(userage);
