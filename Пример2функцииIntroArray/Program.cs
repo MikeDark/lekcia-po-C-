83fcbcb1cf92f14637a5179892059124b79d197d
MikeDark <mkfist2911@gmail.com>
2022-11-06 20:49:26 +0300

    нахождение максимального числа из массива

--- Пример2функцииIntroArray/Program.cs
+++ Пример2функцииIntroArray/Program.cs
@@ -6,5 +6,10 @@
     return result;
 }
 int [] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
-array [0] = 12;
-Console.WriteLine(array [0]);
+//array [0] = 12;
+//Console.WriteLine(array [6]);
+
+int max = Max (Max(array [0],array [1], array [2]), Max(array [3],array [4], array [5]), Max(array [6],array [7], array [8]));
+Console.WriteLine(max);
+
+
