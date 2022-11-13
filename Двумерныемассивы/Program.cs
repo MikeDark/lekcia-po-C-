fea0a7d07d04a34f2d5a339565df3913966528ee
MikeDark <mkfist2911@gmail.com>
2022-11-13 14:38:26 +0300

    построение двумерного массива и заполнения случайными чилами

---
+++ Двумерныемассивы/Program.cs
@@ -0,0 +1,69 @@
+﻿// Создание матрицы
+//string [,] table = new string [2,5];
+//String.Empty
+//table [0,0] table [0,1] table [0,2] table [0,3] table [0,4]
+//table [1,0] table [1,1] table [1,2] table [1,3] table [1,4]
+
+//table[1,2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+//сделаем матрицу но уже с числами и стандартным видом
+// int [,] matrix = new int [3,4];
+// for (int i = 0; i < 3; i++)
+// {
+//     for (int j = 0; j < 4; j++)
+//     {
+//         Console.Write($" {matrix[i, j]} "); 
+//     }
+//     Console.WriteLine();
+// }
+
+//сделаем матрицу но уже с числами и стандартным видом (Вариант 2)
+// int [,] matrix = new int [3,4];
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         Console.Write($" {matrix[i, j]} "); 
+//     }
+//     Console.WriteLine();
+// }
+
+//Заполнение матрицы случайными числами
+// Задача: метод, который будет отдельно печатать двумерную матрицу на экран и заполнять её числами.
+
+void PrintArray(int[,] matr)
+{
+   for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        Console.Write($" {matr[i, j]} "); 
+    }
+    Console.WriteLine();
+}
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random() .Next(1,10); //Генератор случайных псевдочисел
+        }
+    }
+}
+
+int[,] matrix = new int [3,4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
